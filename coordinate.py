c6a1e45dca946c46818ffed2b1e2df86cebcb35d
cain19811028 <cain19811028@gmail.com>
2018-05-19 21:19:12 +0800

    use maps.googleapis.com/maps/api/geocode

--- coordinate.py
+++ coordinate.py
@@ -1,5 +1,12 @@
+import requests
+
+URL = "https://maps.googleapis.com/maps/api/geocode/json?address="
+HEADERS = {'User-Agent': 'Mozilla/5.0'}
+
 def getGeoInfo(city):
-    return city
+    url = URL + city
+    response = requests.get(url, headers = HEADERS)
+    return response
 
 """
 Main
