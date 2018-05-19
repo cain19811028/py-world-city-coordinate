f78bbdabbf2817b6eae29c477d6a656e10a47204
cain19811028 <cain19811028@gmail.com>
2018-05-19 21:31:55 +0800

    add a google api key

--- coordinate.py
+++ coordinate.py
@@ -1,12 +1,15 @@
+import json
 import requests
 
 URL = "https://maps.googleapis.com/maps/api/geocode/json?address="
+KEY = "AIzaSyClwxv44r572Vkcj_MCAfmREF6jdCNZIxA"
 HEADERS = {'User-Agent': 'Mozilla/5.0'}
 
 def getGeoInfo(city):
-    url = URL + city
+    url = URL + city + "&key=" + KEY
     response = requests.get(url, headers = HEADERS)
-    return response
+    result = json.loads(response.text)
+    return result
 
 """
 Main
